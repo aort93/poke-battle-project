6a14add1405b358af5fc85efeb86aa186d5d1c6d
Jessy Hu <jesshuy12@gmail.com>
2019-05-07 18:00:54 -0400

    added sound features

--- src/MoveContainer.js
+++ src/MoveContainer.js
@@ -10,13 +10,13 @@ class MoveContainer extends React.Component {
           <React.Fragment>
           <h3>Trainer's 1 Turn</h3>
           <div className='move-container'>
-            <button id='move1' class="ui button" onClick={() => this.props.handleClick(0)}>{this.props.team1[this.props.team1Idx].moves[0].name}</button>
+            <button id='move1' class="ui button" onClick={() => this.props.handleClick(0, this.props.audio, this.props.audio2)}>{this.props.team1[this.props.team1Idx].moves[0].name}</button>
 
-            <button id='move2' class="ui button" onClick={() => this.props.handleClick(1)}>{this.props.team1[this.props.team1Idx].moves[1].name}</button>
+            <button id='move2' class="ui button" onClick={() => this.props.handleClick(1, this.props.audio, this.props.audio2)}>{this.props.team1[this.props.team1Idx].moves[1].name}</button>
 
-            <button id='move3' class="ui button" onClick={() => this.props.handleClick(2)}>{this.props.team1[this.props.team1Idx].moves[2].name}</button>
+            <button id='move3' class="ui button" onClick={() => this.props.handleClick(2, this.props.audio, this.props.audio2)}>{this.props.team1[this.props.team1Idx].moves[2].name}</button>
 
-            <button id='move4' class="ui button" onClick={() => this.props.handleClick(3)}>{this.props.team1[this.props.team1Idx].moves[3].name}</button>
+            <button id='move4' class="ui button" onClick={() => this.props.handleClick(3, this.props.audio, this.props.audio2)}>{this.props.team1[this.props.team1Idx].moves[3].name}</button>
           </div>
           </React.Fragment>
         :
@@ -24,13 +24,13 @@ class MoveContainer extends React.Component {
           <h3>Trainer's 2 Turn</h3>
           <div className='move-container'>
 
-            <button id='move1' class="ui button" onClick={() => this.props.handleClick(0)}>{this.props.team2[this.props.team2Idx].moves[0].name}</button>
+            <button id='move1' class="ui button" onClick={() => this.props.handleClick(0, this.props.audio, this.props.audio2)}>{this.props.team2[this.props.team2Idx].moves[0].name}</button>
 
-            <button id='move2' class="ui button" onClick={() => this.props.handleClick(1)}>{this.props.team2[this.props.team2Idx].moves[1].name}</button>
+            <button id='move2' class="ui button" onClick={() => this.props.handleClick(1, this.props.audio, this.props.audio2)}>{this.props.team2[this.props.team2Idx].moves[1].name}</button>
 
-            <button id='move3' class="ui button" onClick={() => this.props.handleClick(2)}>{this.props.team2[this.props.team2Idx].moves[2].name}</button>
+            <button id='move3' class="ui button" onClick={() => this.props.handleClick(2, this.props.audio,this.props.audio2)}>{this.props.team2[this.props.team2Idx].moves[2].name}</button>
 
-            <button id='move4' class="ui button" onClick={() => this.props.handleClick(3)}>{this.props.team2[this.props.team2Idx].moves[3].name}</button>
+            <button id='move4' class="ui button" onClick={() => this.props.handleClick(3, this.props.audio, this.props.audio2)}>{this.props.team2[this.props.team2Idx].moves[3].name}</button>
           </div>
           </React.Fragment>
         }
